27cfe031c9cf167d9950e50084b1acb67fdf69ed
Liemaeu <30625460+Liemaeu@users.noreply.github.com>
2019-08-02 11:47:52 +0200

    Update SWBF2ModLoader.py

--- SWBF2ModLoader.py
+++ SWBF2ModLoader.py
@@ -5,6 +5,10 @@ import sys
 #Set default directory
 DefaultDirectory = None
 
+
+ModList = (["BFX", "Battlefront Extreme"], ["TCW", "The Clone Wars"], ["ZZZ", "AI Hero Support"], ["AAA-v1.3patch", "1.3 Patch"], ["RE1", "Realistic Geonosis"])
+
+
 #Checks if a directory is given or a default directory exists
 try:
     if sys.argv[1] != None:
@@ -24,6 +28,13 @@ Mods = list(filter(os.path.isdir, os.listdir()))
 mainwindow = tkinter.Tk()
 mainwindow.title("SWBF2 Mod Loader")
 
+#Function to check if the name of the mod is in the ModList
+def getName(i):
+    for a in range(len(ModList)):
+        if Mods[i] == ModList[a][0]:
+            return ModList[a][1]
+    return Mods[i] #otherwise it uses the name of the folder
+
 #Function to enable a mod
 def Enable(i):
     os.chdir(Directory + str(Mods[i]))
@@ -39,7 +50,7 @@ def Disable(i):
 #Function to create the window
 def create():   
     for i in range(len(Mods)):
-        tkinter.Label(text = Mods[i]).grid(row=i, sticky="nsew", column=0)
+        tkinter.Label(text = getName(i)).grid(row=i, sticky="nsew", column=0)
         os.chdir(Directory + str(Mods[i]))
         if os.path.isfile('addme.script') is True: #Checks if the mod is active or not
             tkinter.Button(text = "Disable", command = lambda a=i:Disable(a), fg="white", bg="red").grid(row=i, sticky="nsew", column=1)
